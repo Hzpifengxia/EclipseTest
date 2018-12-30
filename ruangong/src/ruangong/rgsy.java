35edadab72b91790d0ab1a4cb0e73b2f6d2f4b3f
Hzpifengxia <191760341@qq.com>
2018-12-30 21:30:03 +0800

    First Commit

---
+++ ruangong/src/ruangong/rgsy.java
@@ -0,0 +1,10 @@
+package ruangong;
+
+public class rgsy {
+	public static void main(String args[]) {
+		try{
+			System.out.println(10/0);
+			}catch(ArithmeticException e){
+			System.out.println("Find a error!!!!!!");
+			}
+}}
